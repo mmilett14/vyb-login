96edd6554a6addd20a02b7a803e3dcf9dbb123fb
Mike <mmilett14@gmail.com>
2021-05-07 15:34:29 -0500

    minor changes

--- src/components/accountBox/common.jsx
+++ src/components/accountBox/common.jsx
@@ -12,10 +12,11 @@ export const FormContainer = styled.form`
     width: 100%;
     display: flex;
     flex-direction: column;
+    box-shadow: 0px 0px 2.5px rgba(15, 15, 15, 0.19);
 `;
 
 export const MutedLink = styled.a`
-    font-size: 12px;
+    font-size: 11px;
     color: rgba(200, 200, 200, 0.8);
     font-weight: 500;
     text-decoration: none;
@@ -24,7 +25,7 @@ export const MutedLink = styled.a`
 export const BoldLink = styled.a`
     font-size: 12px;
     color: rgb(241, 196, 15);
-    font-weight: 500;
+    font-weight: 800;
     text-decoration: none;
 `;
 
@@ -35,6 +36,8 @@ export const Input = styled.input`
     border: 1px solid rgba(200, 200, 200, 0.0.3);
     padding: 0px 10px;
     border-bottom: 1.4px solid transparent;
+    transition: all 200ms ease-in-out;
+    font-size: 12px;
 
     &::placeholder {
         color: rgba(200, 200, 200, 1);
